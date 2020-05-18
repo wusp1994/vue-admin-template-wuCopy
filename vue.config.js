3aa285d02fc9ccfc3bdf6ee18a0263e9d4499ac8
wu'shao'peng <812190146@qq.com>
2020-05-18 10:14:03 +0800

    增加反向代理

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const defaultSettings = require('./src/settings.js')
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+	return path.join(__dirname, dir)
 }
 
 const name = defaultSettings.title || 'vue Admin Template' // page title
@@ -17,112 +17,226 @@ const port = process.env.port || process.env.npm_config_port || 9528 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
-  publicPath: '/',
-  outputDir: 'dist',
-  assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'development',
-  productionSourceMap: false,
-  devServer: {
-    port: port,
-    open: true,
-    overlay: {
-      warnings: false,
-      errors: true
-    },
-    before: require('./mock/mock-server.js')
-  },
-  configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
-    name: name,
-    resolve: {
-      alias: {
-        '@': resolve('src')
-      }
-    }
-  },
-  chainWebpack(config) {
-    config.plugins.delete('preload') // TODO: need test
-    config.plugins.delete('prefetch') // TODO: need test
-
-    // set svg-sprite-loader
-    config.module
-      .rule('svg')
-      .exclude.add(resolve('src/icons'))
-      .end()
-    config.module
-      .rule('icons')
-      .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
-      .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
-      .options({
-        symbolId: 'icon-[name]'
-      })
-      .end()
-
-    // set preserveWhitespace
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .loader('vue-loader')
-      .tap(options => {
-        options.compilerOptions.preserveWhitespace = true
-        return options
-      })
-      .end()
-
-    config
-    // https://webpack.js.org/configuration/devtool/#development
-      .when(process.env.NODE_ENV === 'development',
-        config => config.devtool('cheap-source-map')
-      )
-
-    config
-      .when(process.env.NODE_ENV !== 'development',
-        config => {
-          config
-            .plugin('ScriptExtHtmlWebpackPlugin')
-            .after('html')
-            .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
-              inline: /runtime\..*\.js$/
-            }])
-            .end()
-          config
-            .optimization.splitChunks({
-              chunks: 'all',
-              cacheGroups: {
-                libs: {
-                  name: 'chunk-libs',
-                  test: /[\\/]node_modules[\\/]/,
-                  priority: 10,
-                  chunks: 'initial' // only package third parties that are initially dependent
-                },
-                elementUI: {
-                  name: 'chunk-elementUI', // split elementUI into a single package
-                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-                },
-                commons: {
-                  name: 'chunk-commons',
-                  test: resolve('src/components'), // can customize your rules
-                  minChunks: 3, //  minimum common number
-                  priority: 5,
-                  reuseExistingChunk: true
-                }
-              }
-            })
-          config.optimization.runtimeChunk('single')
-        }
-      )
-  }
+	/**
+	 * You will need to set publicPath if you plan to deploy your site under a sub path,
+	 * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
+	 * then publicPath should be set to "/bar/".
+	 * In most cases please use '/' !!!
+	 * Detail: https://cli.vuejs.org/config/#publicpath
+	 */
+	publicPath: '/',
+	outputDir: 'dist',
+	assetsDir: 'static',
+	lintOnSave: process.env.NODE_ENV === 'development',
+	productionSourceMap: false,
+	devServer: {
+		port: port,
+		open: true,
+		overlay: {
+			warnings: false,
+			errors: true
+		},
+		proxy: {
+			// 设置代理
+			'/api': { //定义的代理名
+				target: 'http://192.168.1.194:8184', //接口地址
+				// target: 'http://192.168.1.31:8180',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': ''
+				}
+			},
+			'/Entry': { //定义的代理名
+				target: 'http://192.168.1.194:8184', //接口地址
+				//   target: 'http://192.168.1.31:8181', //接口地址(马驹本地)
+				changeOrigin: true,
+				pathRewrite: {
+					'^/Entry': '/'
+				}
+			},
+			'/analysis': { //定义的代理名，人物分析
+				// target: 'http://192.168.1.194:8184', //接口地址
+				target: 'http://192.168.1.191:8184', //接口地址(美发)
+				changeOrigin: true,
+				pathRewrite: {
+					'^/analysis': '/'
+				}
+			},
+			'/system': { //定义的代理名
+				target: 'http://192.168.1.192:8185', //接口地址
+				// target: 'http://192.168.1.191:8185', //接口地址(美发)
+				changeOrigin: true,
+				pathRewrite: {
+					'^/system': ''
+				}
+			},
+			'/dataMark': { //定义的代理名,数据标注
+				target: 'http://192.168.1.194:8184', //接口地址
+				changeOrigin: true,
+				pathRewrite: {
+					'^/dataMark': '/'
+				}
+			},
+			'/collection': { //定义的代理名,采集管理
+				// target: 'http://192.168.1.193:8182', //接口地址
+				target: 'http://192.168.1.191:8182', //接口地址(美发)
+				changeOrigin: true,
+				pathRewrite: {
+					'^/collection': '/'
+				}
+			},
+			'/fileUpload': { //定义的代理名,文档上传
+				target: 'http://192.168.1.194:8184', //接口地址
+				changeOrigin: true,
+				pathRewrite: {
+					'^/fileUpload': '/'
+				}
+			},
+			// '/qingchuan': { //定义的代理名,文档上传
+			//     target: 'http://113.106.60.18:9091', //接口地址
+			//     changeOrigin: true,
+			//     pathRewrite: {
+			//         '^/qingchuan': '/'
+			//     }
+			// },
+			'/dataQuality': { //定义的代理名,数据质量管理
+				target: 'http://192.168.1.194:8184', //接口地址
+				changeOrigin: true,
+				pathRewrite: {
+					'^/dataQuality': '/'
+				}
+			},
+			'/comment': { //定义的代理名,数据质量管理
+				target: 'http://192.168.1.194:8184', //接口地址
+				changeOrigin: true,
+				pathRewrite: {
+					'^/comment': '/'
+				}
+			},
+			'/qingchuan1': { //定义的代理名,晴川页面接口
+				target: 'http://113.106.60.18:194/qingchuan', //接口地址
+				changeOrigin: true,
+				pathRewrite: {
+					'^/qingchuan1': '/'
+				}
+			},
+			
+			'/recycle': { //定义的代理名,回收站页面接口
+				target: 'http://192.168.1.194:8184/', //接口地址
+				changeOrigin: true,
+				pathRewrite: {
+					// '^/recycle': '/'
+				}
+			},
+			'/dataAuth1': { //定义的代理名, 页面接口
+				target: 'http://192.168.1.194:8184/', //接口地址
+				changeOrigin: true,
+				pathRewrite: {
+					'^/dataAuth1': '/'
+				}
+			},
+			'/proxy928185': { //定义的代理名, 页面接口
+				target: 'http://192.168.1.194:8184/', //接口地址 奕轩
+				changeOrigin: true,
+				pathRewrite: {
+					'^/proxy928185': '/'
+				}
+			},
+			'proxy318184': { //定义的代理名, 页面接口
+				target: 'http://192.168.1.194:8184/', //接口地址 马驹
+				changeOrigin: true,
+				pathRewrite: {
+					'/proxy318184': '/'
+				}
+			}
+		},
+		before: require('./mock/mock-server.js')
+	},
+	configureWebpack: {
+		// provide the app's title in webpack's name field, so that
+		// it can be accessed in index.html to inject the correct title.
+		name: name,
+		resolve: {
+			alias: {
+				'@': resolve('src')
+			}
+		}
+	},
+	chainWebpack(config) {
+		config.plugins.delete('preload') // TODO: need test
+		config.plugins.delete('prefetch') // TODO: need test
+		
+		// set svg-sprite-loader
+		config.module
+		.rule('svg')
+		.exclude.add(resolve('src/icons'))
+		.end()
+		config.module
+		.rule('icons')
+		.test(/\.svg$/)
+		.include.add(resolve('src/icons'))
+		.end()
+		.use('svg-sprite-loader')
+		.loader('svg-sprite-loader')
+		.options({
+			symbolId: 'icon-[name]'
+		})
+		.end()
+		
+		// set preserveWhitespace
+		config.module
+		.rule('vue')
+		.use('vue-loader')
+		.loader('vue-loader')
+		.tap(options => {
+			options.compilerOptions.preserveWhitespace = true
+			return options
+		})
+		.end()
+		
+		config
+		// https://webpack.js.org/configuration/devtool/#development
+		.when(process.env.NODE_ENV === 'development',
+			config => config.devtool('cheap-source-map')
+		)
+		
+		config
+		.when(process.env.NODE_ENV !== 'development',
+			config => {
+				config
+				.plugin('ScriptExtHtmlWebpackPlugin')
+				.after('html')
+				.use('script-ext-html-webpack-plugin', [{
+					// `runtime` must same as runtimeChunk name. default is `runtime`
+					inline: /runtime\..*\.js$/
+				}])
+				.end()
+				config
+				.optimization.splitChunks({
+					chunks: 'all',
+					cacheGroups: {
+						libs: {
+							name: 'chunk-libs',
+							test: /[\\/]node_modules[\\/]/,
+							priority: 10,
+							chunks: 'initial' // only package third parties that are initially dependent
+						},
+						elementUI: {
+							name: 'chunk-elementUI', // split elementUI into a single package
+							priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+							test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+						},
+						commons: {
+							name: 'chunk-commons',
+							test: resolve('src/components'), // can customize your rules
+							minChunks: 3, //  minimum common number
+							priority: 5,
+							reuseExistingChunk: true
+						}
+					}
+				})
+				config.optimization.runtimeChunk('single')
+			}
+		)
+	}
 }
